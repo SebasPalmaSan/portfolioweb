b43286803e678d5b37c541482fc4ee442b5ad76d
SebasPalmaSan <sebapalma59@gmail.com>
2022-06-04 21:23:17 -0300

    agregando proyectos

--- src/ProyectData.js
+++ src/ProyectData.js
@@ -1,4 +1,5 @@
 import MercadoLiebre from './images/MLiebre.jpg'
+import TuLista from './images/tulista.jpg'
 
 const ProyectData = [
     {
@@ -7,6 +8,12 @@ const ProyectData = [
         description: 'Mercado Liebre es un proyecto personal, realizado durante el curso de Programación Web Full Stack en Digital House, simulando un clon de mercado libre.',
         link: 'https://app-mercadoliebre-seb.herokuapp.com/',
     },
+    {
+        title: 'Tu Lista de Compras y Tareas',
+        image: TuLista,
+        description: '',
+        link: 'https://tulista.netlify.app/',
+    }
    
 ]
 export default ProyectData
